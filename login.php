d55b77a83ae24b9ad3ae58dbcd12516c2ab9b288
nsamardzic13 <nikola.samardzic1997@gmail.com>
2018-12-13 13:06:26 +0100

    master

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	$pass = $_POST['passwd'];
 	$mail = $_POST['mail'];
 
-	if( empty($pass) | empty($mail) ) {
+	if(empty($pass) | empty($mail)) {
 		
 	}else{
 		$db = new mysqli('127.0.0.1', 'root', '', 'player_stats');
