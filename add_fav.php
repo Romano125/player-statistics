ab5a75a927099ea71fd6fc3160e2c89236ba780b
romano125 <romano.polic12@gmail.com>
2018-12-19 16:49:17 +0100

    added check if player exists in favs

--- add_fav.php
+++ add_fav.php
@@ -5,9 +5,13 @@
 
 	echo $_SESSION['id'] . ' ' . $_GET['id'];
 
-	$q = "INSERT INTO users_igrac (ID, reg_br_igr) VALUES (" . $_SESSION['id'] . ", '" . $_GET['id'] . "')";
+	$q = "SELECT * FROM users_igrac WHERE reg_br_igr=" . $_GET['id'];
 
-	$db->query($q);
+	if( $db->num_rows == 0 ) {
+		$q = "INSERT INTO users_igrac (ID, reg_br_igr) VALUES (" . $_SESSION['id'] . ", '" . $_GET['id'] . "')";
+
+		$db->query($q);
+	}
 
 	header('Location: app.php');
 ?>
