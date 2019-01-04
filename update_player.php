d687b0ff43cfc4a66877eebf87be904e73037e79
nsamardzic13 <nikola.samardzic1997@gmail.com>
2019-01-04 11:24:19 +0100

    players not allowed to go under zero(goals, assists, etc.)

--- update_player.php
+++ update_player.php
@@ -5,37 +5,39 @@
 	if( isset($_POST['goal+']) ) {
 		$q = "UPDATE igrac SET br_gol=br_gol+1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['goal-']) ) {
+		$tmp = "SELECT br_gol FROM igrac WHERE reg_br_igr='" . $_GET['id'] . "'";
+		if ($tmp < 1) goto flag;
 		$q = "UPDATE igrac SET br_gol=br_gol-1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['ass+']) ) {
-		$q = "UPDATE igrac SET br_asist += 1 WHERE reg_br_igr='" . $_GET['id'] . "'";
+		$q = "UPDATE igrac SET br_asist = br_asist + 1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['ass-']) ) {
+		$tmp = "SELECT br_asist FROM igrac WHERE reg_br_igr='" . $_GET['id'] . "'";
+		if ($tmp < 1) goto flag;
 		$q = "UPDATE igrac SET br_asist=br_asist-1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['yell+']) ) {
 		$q = "UPDATE igrac SET br_zkarton=br_zkarton+1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['yell-']) ) {
+		$tmp = "SELECT br_zkarton FROM igrac WHERE reg_br_igr='" . $_GET['id'] . "'";
+		if ($tmp < 1) goto flag;
 		$q = "UPDATE igrac SET br_zkarton=br_zkarton-1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['red+']) ) {
 		$q = "UPDATE igrac SET br_ckarton=br_ckarton+1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
 	}else if( isset($_POST['red-']) ) {
+		$tmp = "SELECT br_ckarton FROM igrac WHERE reg_br_igr='" . $_GET['id'] . "'";
+		if ($tmp < 1) goto flag;
 		$q = "UPDATE igrac SET br_ckarton=br_ckarton-1 WHERE reg_br_igr='" . $_GET['id'] . "'";
 
-		$db->query($q);
+		
 	}
 
-
+	$db->query($q);
+flag:
 	header('Location: player.php?id=' . $_GET['id']);
 ?>
