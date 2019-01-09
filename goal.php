1c59c6ff85ae40af613a4c8115ba1328e8a6bc87
romano125 <romano.polic12@gmail.com>
2019-01-09 19:49:02 +0100

    goal of the week update solved

--- goal.php
+++ goal.php
@@ -1,9 +1,16 @@
 <?php
 	$link = $_POST['link'];
 
+	$s = '';
+	for( $i = 17; $i < 28; $i++ ) {
+		$s .= $link[$i];
+	}
+
+	echo $s;
+
 	$db = new mysqli('127.0.0.1', 'root', '', 'player_stats');
 
-    $q = "UPDATE goal_of_the_week SET link='" . $link . "'";
+    $q = "UPDATE goal_of_the_week SET link='https://www.youtube.com/embed/" . $s . "'";
 
     $db->query($q);
 
