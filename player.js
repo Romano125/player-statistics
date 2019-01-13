c50b2618cb0d6907c3ba87c1f567cd988428ab11
romano125 <romano.polic12@gmail.com>
2019-01-13 14:43:30 +0100

    something for pop ups

--- player.js
+++ player.js
@@ -14,3 +14,17 @@ document.querySelector('.bg-modal').addEventListener( 'click', () => {
 document.querySelector('.close').addEventListener( 'click', () => {
 	document.querySelector('.bg-modal').style.display = 'none';
 });
+
+/*
+document.getElementById('menu-toggle').addEventListener('click', e => {
+    e.preventDefault();
+    document.getElementById('wrapper').classList.toggle('menuDisplayed');
+});
+ 
+document.getElementById('btn_show').addEventListener( 'click', () => {
+    document.querySelector('.bg-modal').classList.add('is-active');
+});
+ 
+document.querySelector('.close').addEventListener( 'click', () => {
+    document.querySelector('.bg-modal').classList.remove('is-active');
+});*/
